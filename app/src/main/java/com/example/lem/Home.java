0e25b30cd0b40b5b3f2a9b8475e03c2cfc0f2af0
timeo cadouot <timeo.cadouot@C1-P19L.sio-lcdf.edu>
2022-02-24 14:49:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/lem/Home.java
@@ -0,0 +1,41 @@
+package com.example.lem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+import java.io.File;
+
+public class Home extends AppCompatActivity {
+
+    MapView map = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
+        setContentView(R.layout.activity_home);
+        map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(false);
+        map.setMultiTouchControls(true);
+        IMapController mapController = map.getController();
+        mapController.setZoom((int) 17);
+        GeoPoint startPoint = new GeoPoint(47.368284, 1.740004);
+        mapController.setCenter(startPoint);
+    }
+
+}
---
+++ app/src/main/java/com/example/lem/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.lem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void connect(View v) {
+        Intent home = new Intent(this, Home.class);
+        startActivity(home);
+    }
+}
