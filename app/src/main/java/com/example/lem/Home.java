7eeb684138214970096e9354710bbffcbc26d3c8
maxime.dumontet <maxime.dumontet2001@gmail.com>
2022-03-11 08:22:19 +0100

    ajout point map

--- app/src/main/java/com/example/lem/Home.java
+++ app/src/main/java/com/example/lem/Home.java
@@ -1,286 +1,9 @@
-// test tuto
-/*
-package com.example.lem;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.location.Criteria;
-import android.location.LocationProvider;
-//import android.os.StrictMode;
-//import android.support.v4.app.ActivityCompat;
-//import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-
-// before 5.2
-//import org.osmdroid.DefaultResourceProxyImpl;
-//import org.osmdroid.ResourceProxy;
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.Marker;
-import org.osmdroid.views.overlay.Polyline;
-import org.osmdroid.views.overlay.ScaleBarOverlay;
-import org.osmdroid.views.overlay.compass.CompassOverlay;
-import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
-import org.osmdroid.views.overlay.gestures.RotationGestureOverlay;
-import org.osmdroid.views.overlay.infowindow.BasicInfoWindow;
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
-
-public class Home extends AppCompatActivity {
-    LocationManager locationManager = null;
-    private int etat;
-    private String fournisseur;
-    private TextView latitude;
-    private TextView longitude;
-    private TextView Adresse;
-
-    private MapView myOpenMapView;
-    ScaleBarOverlay myScaleBarOverlay;
-    CompassOverlay mCompassOverlay;
-    MyLocationNewOverlay mLocationOverlay;
-    RotationGestureOverlay mRotationGestureOverlay;
-    ArrayList<GeoPoint> trajet;
-
-    Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-
-    LocationListener ecouteurGPS = new LocationListener() {
-        @Override
-        public void onLocationChanged(Location localisation) {
-            Toast.makeText(getApplicationContext(), fournisseur + " localisation", Toast.LENGTH_SHORT).show();
-
-            Log.d("GPS", "localisation : " + localisation.toString());
-            String coordonnees = String.format("Latitude : %f - Longitude : %f\n", localisation.getLatitude(), localisation.getLongitude());
-            Log.d("GPS", coordonnees);
-            String autres = String.format("Vitesse : %f - Altitude : %f - Cap : %f\n", localisation.getSpeed(), localisation.getAltitude(), localisation.getBearing());
-            Log.d("GPS", autres);
-            //String timestamp = String.format("Timestamp : %d\n", localisation.getTime());
-            //Log.d("GPS", "timestamp : " + timestamp);
-            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-            Date date = new Date(localisation.getTime());
-            Log.d("GPS", sdf.format(date));
-
-            String strLatitude = String.format("Latitude : %f", localisation.getLatitude());
-            String strLongitude = String.format("Longitude : %f", localisation.getLongitude());
-            latitude.setText(strLatitude);
-            longitude.setText(strLongitude);
-
-            myOpenMapView.getController().setCenter(new GeoPoint(localisation.getLatitude(), localisation.getLongitude()));
-            //myOpenMapView.setMapOrientation(localisation.getBearing());
-
-            trajet.add(new GeoPoint(localisation.getLatitude(), localisation.getLongitude()));
-
-            *//*Marker tec = new Marker(myOpenMapView);
-            tec.setPosition(new GeoPoint(localisation.getLatitude(), localisation.getLongitude()));
-            tec.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-            tec.setIcon(getResources().getDrawable(R.drawable.trottinette));
-            tec.setTitle("TEC");
-            myOpenMapView.getOverlays().add(tec);*//*
-
-            *//*ArrayList<OverlayItem> items = new ArrayList<OverlayItem>();
-            items.add(new OverlayItem("Title", "Description", new GeoPoint(localisation.getLatitude(), localisation.getLongitude())));
-
-            ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(getApplicationContext(), items,
-                    new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
-                        @Override
-                        public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
-                            Toast.makeText( getApplicationContext(), "Overlay Titled: " +
-                                    item.getTitle() + " Single Tapped" + "\n" + "Description: " +
-                                    item.getSnippet(), Toast.LENGTH_LONG).show();
-                            return true;
-                        }
-                        @Override
-                        public boolean onItemLongPress(final int index, final OverlayItem item) {
-                            return false;
-                        }
-                    });
-            //mOverlay.setFocusItemsOnTap(true);
-            myOpenMapView.getOverlays().add(mOverlay);*//*
-
-            myOpenMapView.invalidate();
-
-            List<Address> adresses = null;
-            try {
-                adresses = geocoder.getFromLocation(localisation.getLatitude(), localisation.getLongitude(), 1);
-            } catch (IOException ioException) {
-                Log.e("GPS", "erreur", ioException);
-            } catch (IllegalArgumentException illegalArgumentException) {
-                Log.e("GPS", "erreur " + coordonnees, illegalArgumentException);
-            }
-
-            if (adresses == null || adresses.size()  == 0) {
-                Log.e("GPS", "erreur aucune adresse !");
-            } else {
-                Address adresse = adresses.get(0);
-                ArrayList<String> addressFragments = new ArrayList<String>();
-
-                String strAdresse = adresse.getAddressLine(0) + ", " + adresse.getLocality();
-                Log.d("GPS", "adresse : " + strAdresse);
-
-                for(int i = 0; i <= adresse.getMaxAddressLineIndex(); i++) {
-                    addressFragments.add(adresse.getAddressLine(i));
-                }
-                Log.d("GPS", TextUtils.join(System.getProperty("line.separator"), addressFragments));
-                Adresse.setText(TextUtils.join(System.getProperty("line.separator"), addressFragments));
-            }
-        }
-
-        @Override
-        public void onProviderDisabled(String fournisseur) {
-            Toast.makeText(getApplicationContext(), fournisseur + " désactivé !", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onProviderEnabled(String fournisseur) {
-            Toast.makeText(getApplicationContext(), fournisseur + " activé !", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onStatusChanged(String fournisseur, int status, Bundle extras) {
-            if (etat != status) {
-                switch (status) {
-                    case LocationProvider.AVAILABLE:
-                        Toast.makeText(getApplicationContext(), fournisseur + " état disponible", Toast.LENGTH_SHORT).show();
-                        break;
-                    case LocationProvider.OUT_OF_SERVICE:
-                        Toast.makeText(getApplicationContext(), fournisseur + " état indisponible", Toast.LENGTH_SHORT).show();
-                        break;
-                    case LocationProvider.TEMPORARILY_UNAVAILABLE:
-                        Toast.makeText(getApplicationContext(), fournisseur + " état temporairement indisponible", Toast.LENGTH_SHORT).show();
-                        break;
-                    default:
-                        Toast.makeText(getApplicationContext(), fournisseur + " état : " + status, Toast.LENGTH_SHORT).show();
-                }
-            }
-            etat = status;
-        }
-    };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-        etat = 0;
-        latitude = (TextView) findViewById(R.id.textViewLatitude);
-        longitude = (TextView) findViewById(R.id.textViewLongitude);
-        Adresse = (TextView) findViewById(R.id.textViewAdresse);
-
-        myOpenMapView = (MapView)findViewById(R.id.map);
-        myOpenMapView.setBuiltInZoomControls(true);
-        myOpenMapView.setClickable(true);
-        myOpenMapView.getController().setZoom(18);
-
-        myScaleBarOverlay = new ScaleBarOverlay(myOpenMapView);
-        myOpenMapView.getOverlays().add(myScaleBarOverlay);
-
-        mCompassOverlay = new CompassOverlay(getApplicationContext(), new InternalCompassOrientationProvider(getApplicationContext()), myOpenMapView);
-        mCompassOverlay.enableCompass();
-        myOpenMapView.getOverlays().add(mCompassOverlay);
-
-        mRotationGestureOverlay = new RotationGestureOverlay(getApplicationContext(), myOpenMapView);
-        mRotationGestureOverlay.setEnabled(true);
-        myOpenMapView.setMultiTouchControls(true);
-        myOpenMapView.getOverlays().add(this.mRotationGestureOverlay);
-
-        trajet = new ArrayList<GeoPoint>();
-
-        Log.d("GPS", "onCreate");
-
-        initialiserLocalisation();
-
-        mLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(getApplicationContext()), myOpenMapView);
-        mLocationOverlay.enableMyLocation();
-        myOpenMapView.setMultiTouchControls(true);
-        myOpenMapView.getOverlays().add(mLocationOverlay);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        arreterLocalisation();
-    }
-
-    private void initialiserLocalisation() {
-        if (locationManager == null) {
-            locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
-            Criteria criteres = new Criteria();
-
-            // la précision  : (ACCURACY_FINE pour une haute précision ou ACCURACY_COARSE pour une moins bonne précision)
-            criteres.setAccuracy(Criteria.ACCURACY_FINE);
-
-            // l'altitude
-            criteres.setAltitudeRequired(true);
-
-            // la direction
-            criteres.setBearingRequired(true);
-
-            // la vitesse
-            criteres.setSpeedRequired(true);
-
-            // la consommation d'énergie demandée
-            criteres.setCostAllowed(true);
-            //criteres.setPowerRequirement(Criteria.POWER_HIGH);
-            criteres.setPowerRequirement(Criteria.POWER_MEDIUM);
-
-            fournisseur = locationManager.getBestProvider(criteres, true);
-            Log.d("GPS", "fournisseur : " + fournisseur);
-        }
-
-        if (fournisseur != null) {
-            // dernière position connue
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                Log.d("GPS", "no permissions !");
-                return;
-            }
-
-            Location localisation = locationManager.getLastKnownLocation(fournisseur);
-            if(localisation != null) {
-                // on notifie la localisation
-                ecouteurGPS.onLocationChanged(localisation);
-            }
-
-            // on configure la mise à jour automatique : au moins 10 mètres et 15 secondes
-            locationManager.requestLocationUpdates(fournisseur, 15000, 10, ecouteurGPS);
-        }
-    }
-
-    private void arreterLocalisation() {
-        if(locationManager != null) {
-            locationManager.removeUpdates(ecouteurGPS);
-            ecouteurGPS = null;
-        }
-    }
-}*/
-
-
-
-// Partie qui fonctionne presque
 package com.example.lem;
 
 import static java.security.AccessController.getContext;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
 import android.content.ContextWrapper;
@@ -291,6 +14,7 @@ import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
@@ -299,6 +23,7 @@ import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.ItemizedIconOverlay;
 import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.OverlayItem;
 
 import java.io.File;
@@ -307,10 +32,18 @@ import java.util.ArrayList;
 public class Home extends AppCompatActivity {
 
     MapView map = null;
+    private ViewPager viewPager;
+    private ViewPageAdapter viewPageAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //test swip
+        viewPager = findViewById(R.id.viewpage);
+        viewPageAdapter = new ViewPageAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(viewPageAdapter);
 
         // load/initialize the osmdroid configuration
         Context ctx = getApplicationContext();
@@ -318,10 +51,10 @@ public class Home extends AppCompatActivity {
 
         // inflate and create the map
         setContentView(R.layout.activity_home);
-        map = (MapView) findViewById(R.id.map);
+        map = (MapView) findViewById(R.id.mapview);
         map.setTileSource(TileSourceFactory.MAPNIK);
 
-        // default zoom buttons and ability to zoom with 2 fingers (multi-touch)
+        // default zoom buttons and ability to zoom with 2 fingers (multi-touch) (use ctrl on web)
         map.setBuiltInZoomControls(false);
         map.setMultiTouchControls(true);
 
@@ -329,41 +62,26 @@ public class Home extends AppCompatActivity {
         IMapController mapController = map.getController();
         mapController.setZoom((int) 17);
         GeoPoint startPoint = new GeoPoint(47.368284, 1.740004);
+        GeoPoint startPoint1 = new GeoPoint(47.370524, 1.738388);
         mapController.setCenter(startPoint);
 
+        Marker startMarker = new Marker(map);
+        Marker startMarker1 = new Marker(map);
+
+        startMarker.setPosition(startPoint);
+        startMarker1.setPosition(startPoint1);
+
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        startMarker1.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+
+        map.getOverlays().add(startMarker);
+        map.getOverlays().add(startMarker1);
+
+        startMarker.setIcon(getResources().getDrawable(R.drawable.marker));
+        startMarker.setTitle("Start point");
+
+        startMarker1.setIcon(getResources().getDrawable(R.drawable.marker));
+        startMarker1.setTitle("Start point");
+
     }
-
-    /*// add MapView
-
-    // Il faut un système pour repérer l'appareil (http://tvaira.free.fr/dev/android/android-geolocalisation.html)
-
-    // Il faut un système pour intégrer la map et pouvoir mettre des points dessus (http://tvaira.free.fr/dev/android/android-carte.html)
-
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        MapView mMapView = new MapView(inflater.getContext(), map.getTileProvider(), map.getContext());
-        return mMapView;
-
-        // ping system
-        //your items
-        ArrayList<OverlayItem> items = new ArrayList<OverlayItem>();
-        items.add(new OverlayItem("Title", "Description", new GeoPoint(0.0d,0.0d))); // Lat/Lon decimal degrees
-
-        //the overlay
-        Context context = null;
-        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(items,
-                new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
-                    @Override
-                    public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
-                        //do something
-                        return true;
-                    }
-                    @Override
-                    public boolean onItemLongPress(final int index, final OverlayItem item) {
-                        return false;
-                    }
-                }, context);
-        mOverlay.setFocusItemsOnTap(true);
-
-        mMapView.getOverlays().add(mOverlay);
-    }*/
 }
